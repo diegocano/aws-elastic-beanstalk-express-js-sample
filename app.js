0a2abc76d26c938fd18c61637f769eca207fe8d6
diegocano <cpucano@gmail.com>
2022-07-06 23:46:23 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! by Diego vamos de una vez'));!
+app.get('/', (req, res) => res.send('Hello World! aws kano kaws'));!
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
